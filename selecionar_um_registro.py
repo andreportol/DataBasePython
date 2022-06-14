76b63c0aa325461fd2c94b02daf1477b58ebd051
andreportol <andreportol@gmail.com>
2022-06-14 09:46:33 -0400

    Adicionado o selecionar_um_registro.py

---
+++ selecionar_um_registro.py
@@ -0,0 +1,33 @@
+import psycopg2
+
+def conecta_DB():
+    try:
+        conexao = psycopg2.connect(
+            host = 'localhost', 
+            database = 'Python', 
+            user = 'postgres',
+            password = 'postgres')
+    except psycopg2.ProgrammingError as e:
+        print(f'Erro: {e}')
+    else:
+        print('Banco conectado!')
+        return conexao
+
+if __name__=='__main__':
+    try:
+        conexao = conecta_DB()
+        sql = '''select nome, telefone from contatos 
+                 offset 22'''
+        cursor = conexao.cursor()
+        cursor.execute(sql)
+    except psycopg2.ProgrammingError as e:
+        print(f'Erro: {e}')
+    else:
+            print(f'registro:{cursor.fetchone()}')
+            print(f'registro:{cursor.fetchone()}')
+            print(f'registro:{cursor.fetchone()}')
+            print(f'registro:{cursor.fetchone()}')
+            print(f'registro:{cursor.fetchone()}')
+            print(f'registro:{cursor.fetchone()}')
+            print(f'registro:{cursor.fetchone()}')
+    conexao.close()
