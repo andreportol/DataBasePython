3f688c976978d3f618469200bff0ae437c34e528
andreportol <andreportol@gmail.com>
2022-06-14 08:31:10 -0400

    Programa inserirDados.py adicionado

---
+++ inserirDados.py
@@ -0,0 +1,24 @@
+import psycopg2
+
+def conexao_DB():
+    try:
+        conexao = psycopg2.connect(host='localhost', 
+                            database='Python',
+                            user='postgres', 
+                            password='postgres')
+    except psycopg2.OperationalError:
+        print('Banco não conectado!')
+    else:
+        print('Banco de dados conectado!')
+        return conexao
+
+sql = ''' insert into contatos (nome, telefone)
+          values (%s,%s)'''
+args = ('André', '999391551')
+
+conexao = conexao_DB()
+cursor = conexao.cursor()
+cursor.execute(sql,args)
+conexao.commit()
+print('1 registro incluido, ID: ', cursor.lastrowid)
+conexao.close()
