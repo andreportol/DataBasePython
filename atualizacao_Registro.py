10bcd1a1096a469011780956c11c816e2b464a46
andreportol <andreportol@gmail.com>
2022-07-07 14:40:38 -0400

    Programa finalizado.

---
+++ atualizacao_Registro.py
@@ -0,0 +1,32 @@
+import psycopg2
+
+def conexao_DB(): 
+    try:
+        conexao = psycopg2.connect(host='localhost', 
+                            database='Python',
+                            user='postgres', 
+                            password='Genius2003$')
+    except psycopg2.OperationalError:
+        print('Banco não conectado!')
+    else:
+        print('Banco de dados conectado!')
+        return conexao
+
+conexao = conexao_DB()
+cursor = conexao.cursor()
+
+
+sql = '''update contatos
+         set telefone = 9992520749 
+         where id = %s'''
+# telefone é integer sem apostrofo
+sql1 = ''' update contatos
+           set nome = 'Andre Porto'
+           where  id = %s '''
+# Nome é String com apostrofo
+
+id = input("Digite o id do contato que será atualizado: ")
+args = (id)
+cursor.execute(sql1,id)
+conexao.commit()
+conexao.close()
