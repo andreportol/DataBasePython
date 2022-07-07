573590134cc164c2d103e23b69554c6ddbb387b0
andreportol <andreportol@gmail.com>
2022-07-07 14:18:47 -0400

    Finalizado o programa para testar o comando like.

---
+++ selecionar_com_filtro.py
@@ -0,0 +1,25 @@
+import psycopg2
+
+def conexao_DB(): 
+    try:
+        conexao = psycopg2.connect(host='localhost', 
+                            database='Python',
+                            user='postgres', 
+                            password='Genius2003$')
+    except psycopg2.OperationalError:
+        print('Banco não conectado!')
+    else:
+        print('Banco de dados conectado!')
+        return conexao
+
+conexao = conexao_DB()
+cursor = conexao.cursor()
+sql = '''select * from contatos
+        where telefone like '%11%' '''
+cursor.execute(sql)
+contatos = cursor.fetchall()
+for i, contato in enumerate (contatos, start = 1):
+    print(f'{i} = {contato[0]},{contato[1]}')
+
+conexao.commit()
+conexao.close()
